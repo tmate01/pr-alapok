e15d1728a74021321d2554c2451c14f0877d6d1b
tmate01 <113334902+tmate01@users.noreply.github.com>
2023-10-09 13:07:31 +0200

    Add files via upload

---
+++ szamonkeres_01.py
@@ -0,0 +1,17 @@
+print("Tóth Máté Zsolt")
+szam1 =int(input("kérek egy jegyet 1 és 5 között!")) 
+if szam1 == 5:
+    print("A jegyed jeles!")
+if szam1 == 4:
+    print("A jegyed jó!")
+if szam1 == 3:
+    print("A jegyed közepes!")
+if szam1 == 2:
+    print("A jegyed elégséges!")
+if szam1 == 1:
+    print("A jegyed elégtelen!")
+
+
+
+
+
